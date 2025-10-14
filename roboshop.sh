fc8ba20aa0e9bf0941e2672a9970e58854bdbeff
lavanyak-hub <lavanyakotha1999@gmail.com>
2025-10-14 21:32:00 +0530

    some message

--- roboshop.sh
+++ roboshop.sh
@@ -14,7 +14,7 @@ do
   else
    IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
    fi
-   echo "instance: $IP"
+   echo "$instance: $IP"
 
    done
 
