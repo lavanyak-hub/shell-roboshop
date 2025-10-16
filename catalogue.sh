3e895656b7c50a2055c6703fbdeec89b4f92afc6
lavanyak-hub <lavanyakotha1999@gmail.com>
2025-10-16 17:13:48 +0530

    some message

--- catalogue.sh
+++ catalogue.sh
@@ -77,8 +77,10 @@ VALIDATE $? "Copy mongo repo"
 
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Install MongoDB client"
+
 mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
 VALIDATE $? "Load catalogue products"
+
 systemctl restart catalogue
 VALIDATE $? "Restarted catalogue"
 
