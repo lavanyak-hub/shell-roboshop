5b7cd0e3cca427c3211d9adcd6e9ae08c9a31aba
lavanyak-hub <lavanyakotha1999@gmail.com>
2025-10-18 15:16:56 +0530

    some message

--- mysql.sh
+++ mysql.sh
@@ -31,20 +31,14 @@ else
     fi
 }
 
-dnf module disable redis -y &>>$LOG_FILE
-VALIDATE $? "Desabling Dfault Redis"
-dnf module enable redis:7 -y &>>$LOG_FILE
-VALIDATE $? "Enabling Redis 7"
-dnf install redis -y &>>$LOG_FILE
-VALIDATE $? "Installing Redis"
-
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
-VALIDATE $? "Allowing Remote connections to Redis"
-
-systemctl enable redis &>>$LOG_FILE
-VALIDATE $? "Enabling Redis"
-systemctl start redis &>>$LOG_FILE
-VALIDATE $? "Starting Redis"
+dnf install mysql-server -y
+VALIDATE $? "Installing MySQL Server"
+systemctl enable mysqld
+VALIDATE $? "Enabling MySQL Server"
+systemctl start mysqld
+VALIDATE $? "Starting MySQL Server"
+mysql_secure_installation --set-root-pass RoboShop@1
+VALIDATE $? "Setting up Root password
 
 END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
