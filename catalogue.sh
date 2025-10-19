e7a75b0b216ca59e0ed814a4b91d963b94bc4756
lavanyak-hub <lavanyakotha1999@gmail.com>
2025-10-19 17:33:16 +0530

    Some message

--- catalogue.sh
+++ catalogue.sh
@@ -42,7 +42,7 @@ VALIDATE $? "Installing NodeJS"
 
 id roboshop &>>$LOG_FILE
 if [ $? -ne 0 ]; then
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
     VALIDATE $? "Creating system user"
 else
     echo -e "User already exist ... $Y SKIPPING $N"
